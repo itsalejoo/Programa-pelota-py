0d22d687da7d55f537f5459e3d4fcd0264db7f6f
alejandro sanchez <kealsansab@gmail.com>
2024-03-27 22:22:25 -0500

    pelota_1

---
+++ Pelota.py
@@ -0,0 +1,19 @@
+class pelota: 
+    def _init_(self):
+        self.x=0
+        self.y=0
+        self.radio=1
+        self.color="red"
+        self.tipo="circulo"
+        self.tipoRelleno="relleno"
+    def mover (self):
+        print("me estoy moviendo")  
+    
+    def rebotar(self):
+        pass 
+
+    def rebotar(self):
+        pass 
+
+    def dibujar(self):
+        pass 
